b7f9f82865be2674f8029769f58fd37a92abbeac
vishhnuvardhann <vr7987@srmist.edu.in>
2026-02-23 13:52:54 +0530

    uc2 push

---
+++ UC2.java
@@ -0,0 +1,20 @@
+public class UC2 {
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println(" *******  " + " *******  " + " *******  " + "  *****  ");
+        // Line 2
+        System.out.println("*       * " + "*       * " + "*       * " + " *     * ");
+        // Line 3
+        System.out.println("*       * " + "*       * " + "*       * " + "  *      ");
+        // Line 4
+        System.out.println("*       * " + "*       * " + "******** " + "   ***   ");
+        // Line 5
+        System.out.println("*       * " + "*       * " + "*        " + "      *  ");
+        // Line 6
+        System.out.println("*       * " + "*       * " + "*        " + " *     * ");
+        // Line 7
+        System.out.println(" *******  " + " *******  " + "*        " + "  *****  ");
+
+    }
+}
